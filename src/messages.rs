3703e83802c5ce6fe36e09b1aec08bf37beb75b5
PokeyLink227 <thomasrice7@yahoo.com>
2025-05-13 06:08:37 -0400

    made message parse avoid copying piece data

--- src/messages.rs
+++ src/messages.rs
@@ -1,55 +1,67 @@
 use bitvec::order::Msb0;
-use bitvec::prelude::BitVec;
-use byteorder::{NetworkEndian, ReadBytesExt, WriteBytesExt};
-use std::io;
-use std::io::{Read, Write, Result, Seek};
+use bitvec::prelude::{BitSlice, BitVec};
+use bitvec::view::BitView;
+use byteorder::{ByteOrder, NetworkEndian, ReadBytesExt, WriteBytesExt};
+use std::io::{self, Read, Result, Seek, Write};
 
-pub struct Have { // <len=0005><id=4><piece index>
-    pub piece_index : u32
+#[derive(Debug)]
+pub struct Have {
+    // <len=0005><id=4><piece index>
+    pub piece_index: u32,
 }
 
-pub struct Bitfield { // <len=0001+X><id=5><bitfield>
-    pub bitfield : BitVec<u8, Msb0>
+#[derive(Debug)]
+pub struct Bitfield<'a> {
+    // <len=0001+X><id=5><bitfield>
+    pub bitfield: &'a BitSlice<u8, Msb0>,
 }
 
-pub struct Request { // <len=0013><id=6><index><begin><length>
-    pub index : u32,
-    pub begin : u32,
-    pub length : u32
+#[derive(Debug)]
+pub struct Request {
+    // <len=0013><id=6><index><begin><length>
+    pub index: u32,
+    pub begin: u32,
+    pub length: u32,
 }
 
-pub struct Piece { // <len=0009+X><id=7><index><begin><block>
-    pub index : u32,
-    pub begin : u32,
-    pub block : Vec<u8> // TODO - rethink this data type
+#[derive(Debug)]
+pub struct Piece<'a> {
+    // <len=0009+X><id=7><index><begin><block>
+    pub index: u32,
+    pub begin: u32,
+    pub block: &'a [u8], // TODO - rethink this data type
 }
 
-pub struct Cancel { // <len=0013><id=8><index><begin><length>
-    pub index : u32,
-    pub begin : u32,
-    pub length : u32
+#[derive(Debug)]
+pub struct Cancel {
+    // <len=0013><id=8><index><begin><length>
+    pub index: u32,
+    pub begin: u32,
+    pub length: u32,
 }
 
-pub struct Port { // <len=0003><id=9><listen-port>
-    pub port : u16
+#[derive(Debug)]
+pub struct Port {
+    // <len=0003><id=9><listen-port>
+    pub port: u16,
 }
 
-pub enum Message {
-    KeepAlive, // <len=0000>
-    Choke, // <len=0001><id=0>
-    UnChoke, // <len=0001><id=1>
-    Interested, // <len=0001><id=2>
-    NotInterested, // <len=0001><id=3>
-    Have(Have), // <len=0005><id=4><piece index>
-    Bitfield(Bitfield), // <len=0001+X><id=5><bitfield>
-    Request(Request), // <len=0013><id=6><index><begin><length>
-    Piece(Piece), // <len=0009+X><id=7><index><begin><block>
-    Cancel(Cancel), // <len=0013><id=8><index><begin><length>
-    Port(Port), // <len=0003><id=9><listen-port>
+#[derive(Debug)]
+pub enum Message<'a> {
+    KeepAlive,              // <len=0000>
+    Choke,                  // <len=0001><id=0>
+    UnChoke,                // <len=0001><id=1>
+    Interested,             // <len=0001><id=2>
+    NotInterested,          // <len=0001><id=3>
+    Have(Have),             // <len=0005><id=4><piece index>
+    Bitfield(Bitfield<'a>), // <len=0001+X><id=5><bitfield>
+    Request(Request),       // <len=0013><id=6><index><begin><length>
+    Piece(Piece<'a>),       // <len=0009+X><id=7><index><begin><block>
+    Cancel(Cancel),         // <len=0013><id=8><index><begin><length>
+    Port(Port),             // <len=0003><id=9><listen-port>
 }
 
-impl Message {
-
+impl<'a> Message<'a> {
     // <length prefix><message ID><payload>
     // 1. length prefix is a four byte big-endian value
     // 2. message ID is a single decimal byte
@@ -78,7 +90,7 @@ impl Message {
             }
             Message::Bitfield(substance) => {
                 writer.write_u8(5)?;
-                writer.write_all(substance.bitfield.as_raw_slice())?;
+                writer.write_all(substance.bitfield.to_bitvec().as_raw_slice())?;
             }
             Message::Request(substance) => {
                 writer.write_u8(6)?;
@@ -112,62 +124,83 @@ impl Message {
         Ok(size as usize)
     }
 
-    pub fn parse<T: Read>(mut reader: T) -> Result<Self> {
-        let size = reader.read_u32::<NetworkEndian>()?;
+    pub fn parse(buf: &'a [u8]) -> Result<Self> {
+        let size = NetworkEndian::read_u32(&buf[0..4]);
 
         if size == 0 {
             return Ok(Message::KeepAlive);
         }
 
-        let msg_type = reader.read_u8()?;
+        let msg_type = buf[4];
 
         match msg_type {
-            0 => { // choke
-                return Ok(Message::Choke); 
+            0 => {
+                // choke
+                return Ok(Message::Choke);
             }
-            1 => { // unchoke
-                return Ok(Message::UnChoke); 
+            1 => {
+                // unchoke
+                return Ok(Message::UnChoke);
             }
-            2 => { // interested
-                return Ok(Message::Interested); 
+            2 => {
+                // interested
+                return Ok(Message::Interested);
             }
-            3 => { // not interested
-                return Ok(Message::NotInterested); 
+            3 => {
+                // not interested
+                return Ok(Message::NotInterested);
             }
-            4 => { // have
-                let piece_index : u32 = reader.read_u32::<NetworkEndian>()?;
-                return Ok(Message::Have(Have {piece_index})); 
+            4 => {
+                // have
+                let piece_index: u32 = NetworkEndian::read_u32(&buf[5..9]);
+                return Ok(Message::Have(Have { piece_index }));
             }
-            5 => { // bitfield
+            5 => {
+                // bitfield
                 // we know 'size' is a u32 representing 1 + X
                 // where X is the number of bytes that makes up this bitfield
-                let mut bitfield_bytes : Vec<u8> = vec![0; (size-1) as usize];
-                reader.read_exact(&mut bitfield_bytes)?;
-                let bitfield = BitVec::<u8, Msb0>::from_vec(bitfield_bytes);
-                return Ok(Message::Bitfield(Bitfield {bitfield})); 
+                let bitfield: &BitSlice<u8, Msb0> =
+                    buf[5..5 + size as usize - 1].view_bits::<Msb0>();
+                return Ok(Message::Bitfield(Bitfield { bitfield }));
             }
-            6 => { // request
-                let index : u32 = reader.read_u32::<NetworkEndian>()?;
-                let begin : u32 = reader.read_u32::<NetworkEndian>()?;
-                let length : u32 = reader.read_u32::<NetworkEndian>()?;
-                return Ok(Message::Request(Request {index, begin, length})); 
+            6 => {
+                // request
+                let index: u32 = NetworkEndian::read_u32(&buf[5..9]);
+                let begin: u32 = NetworkEndian::read_u32(&buf[9..13]);
+                let length: u32 = NetworkEndian::read_u32(&buf[13..17]);
+                return Ok(Message::Request(Request {
+                    index,
+                    begin,
+                    length,
+                }));
             }
-            7 => { // piece
-                let index : u32 = reader.read_u32::<NetworkEndian>()?;
-                let begin : u32 = reader.read_u32::<NetworkEndian>()?;
+            7 => {
+                // piece
+                let index: u32 = NetworkEndian::read_u32(&buf[5..9]);
+                let begin: u32 = NetworkEndian::read_u32(&buf[9..13]);
                 // len is 9 + X
-                let mut block : Vec<u8> = vec![0; (size-9) as usize];
-                reader.read_exact(&mut block)?;
-                return Ok(Message::Piece(Piece {index, begin, block})); 
+                let len = size as usize - 9;
+                //reader.read_exact(&mut block)?;
+                return Ok(Message::Piece(Piece {
+                    index,
+                    begin,
+                    block: &buf[13..13 + len],
+                }));
             }
-            8 => { // cancel
-                let index : u32 = reader.read_u32::<NetworkEndian>()?;
-                let begin : u32 = reader.read_u32::<NetworkEndian>()?;
-                let length : u32 = reader.read_u32::<NetworkEndian>()?;
-                return Ok(Message::Cancel(Cancel {index, begin, length})); 
+            8 => {
+                // cancel
+                let index: u32 = NetworkEndian::read_u32(&buf[5..9]);
+                let begin: u32 = NetworkEndian::read_u32(&buf[9..13]);
+                let length: u32 = NetworkEndian::read_u32(&buf[13..17]);
+                return Ok(Message::Cancel(Cancel {
+                    index,
+                    begin,
+                    length,
+                }));
             }
-            9 => { // port
-                return Ok(Message::UnChoke); 
+            9 => {
+                // port
+                return Ok(Message::UnChoke);
             }
             _ => Err(io::Error::new(
                 io::ErrorKind::InvalidInput,
@@ -175,9 +208,7 @@ impl Message {
             )),
         }
     }
-
 }
-
 
 // NOTE: Use `cargo test -- --show-output`.
 #[cfg(test)]
@@ -204,3 +235,4 @@ mod tests {
         // dbg!(tr.encode_http_get("test".into()));
     }
 }
+
